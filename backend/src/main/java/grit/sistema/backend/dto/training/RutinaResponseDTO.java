03589c4fd1a74ac46d5be6700248ceeea0df5cf7
kevincal5 <keizyzanc@gmail.com>
2026-05-03 10:43:11 +0200

    DEV: Añadir los campos faltantes a la respuesta de RutinaDTO.

--- backend/src/main/java/grit/sistema/backend/dto/training/RutinaResponseDTO.java
+++ backend/src/main/java/grit/sistema/backend/dto/training/RutinaResponseDTO.java
@@ -1,11 +1,16 @@
 package grit.sistema.backend.dto.training;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record RutinaResponseDTO(
         UUID id,
+        UUID atletaId,
+        String nombre,
+        String descripcion,
+        boolean activo,
         OffsetDateTime creadoEn,
-        UUID atletaId
+        List<SesionRutinaDTO> sesiones
 ) {
 }
