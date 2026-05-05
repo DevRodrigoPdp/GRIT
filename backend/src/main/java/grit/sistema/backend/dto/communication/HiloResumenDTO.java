8353efc79b645b865bb14774065458b59412395c
02555051 <kevin.calderon@ccoo.es>
2026-05-05 17:10:02 +0200

    FIX: Arreglar error de persistencia JPA al renombrar el nombre de las variables del dto de la consulta de listar los hilos.

--- backend/src/main/java/grit/sistema/backend/dto/communication/HiloResumenDTO.java
+++ backend/src/main/java/grit/sistema/backend/dto/communication/HiloResumenDTO.java
@@ -11,7 +11,7 @@ public record HiloResumenDTO(
         String titulo,
         CategoriaHilo categoria,
         ContextoHilo contexto,
-        String de, // "ENTRENADOR" | "ATLETA"
+        String creadoPor, // "ENTRENADOR" | "ATLETA"
         LocalDateTime fechaAbierto,
         Long totalMensajes,
         UltimoMensajeDTO ultimoMensaje,
