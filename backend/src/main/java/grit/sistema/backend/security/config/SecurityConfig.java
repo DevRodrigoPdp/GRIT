db26a3dc13d414f94b95f76dd8e518240f5ec48c
Rodrigo <rpuertadelpozo@gmail.com>
2026-05-18 23:01:27 +0200

    url

--- backend/src/main/java/grit/sistema/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/grit/sistema/backend/security/config/SecurityConfig.java
@@ -114,7 +114,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(Collections.singletonList("https://grit-frontend-beta.vercel.app"));
+        config.setAllowedOrigins(Collections.singletonList("https://frontend-rho-two-49.vercel.app/"));
 
         // Métodos permitidos
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
