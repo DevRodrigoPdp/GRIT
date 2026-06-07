c4d9405fe52af951cf1d25ac02f02b2f10a6a20b
kevincal5 <keizyzanc@gmail.com>
2026-06-07 21:50:27 +0200

    FIX(docker): Restablecer la configuración.

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 set -e
 
-# 1. Borramos la configuración por defecto y el index de bienvenida
-rm -f /etc/nginx/conf.d/default.conf
-rm -f /usr/share/nginx/html/index.html
-
-# 2. Inyectamos las variables.
-# IMPORTANTE: Solo pasamos las variables que queremos sustituir
-envsubst '$BACKEND_URL $PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
-
-echo "--- Configuración final generada ---"
-cat /etc/nginx/conf.d/default.conf
+# Sustituye solo $BACKEND_URL en el template, deja las variables de nginx intactas
+envsubst '$BACKEND_URL $PORT $DNS_RESOLVER' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
